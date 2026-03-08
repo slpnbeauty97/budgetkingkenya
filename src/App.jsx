8dc22084c74ca0235b9cc51321b2717d11402f14
slpnbeauty97 <chrismacharia65@yahoo.com>
2026-03-08 15:09:22 +0300

    try this layoout fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./Layout/MainLayout";
 
 
 import Budget from "./pages/Budget"
