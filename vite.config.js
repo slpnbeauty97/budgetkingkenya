2bad6cf373b617c82322ea6d990961c355c6f5e0
Chris Macharia <121160927+slpnbeauty97@users.noreply.github.com>
2026-03-11 19:58:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allow network access
+    port: 5173, // optional, default Vite port
+  },
+  preview: {
+    allowedHosts: ['budgetking.online'], // ✅ allow your custom host
+  },
+});
